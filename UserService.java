df1057f30647bc635bc8fc61a0a2aaeb51221031
moerpooo <46314180+moerpooo@users.noreply.github.com>
2022-06-08 13:58:21 +0800

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,13 @@
+package com.yong.service;
+
+import com.yong.spring.Component;
+
+/**
+ * @Classname UserService
+ * @Description TODO
+ * @Date 2022/6/7 13:42
+ * @Created by Banging
+ */
+@Component("userService")
+public class UserService {
+}
